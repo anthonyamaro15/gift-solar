13dbb3f1c6559402d854f657569f39539b07d1dc
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-08-19 18:42:26 -0700

    added header and navbar

---
+++ src/components/MainApp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainHomePage from "./MarketingPage/MainHomePage";
+
+const MainApp = () => {
+  return (
+    <div>
+      <MainHomePage />
+    </div>
+  );
+};
+
+export default MainApp;
---
+++ src/components/MarketingPage/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="Header">
+      <div className="Header-wrapper">
+        <h1>en gift solar puedes obtener mas por menos</h1>
+        <p>
+          Ofrecemos 3 exelentes paquetes donde incluye un extra para su salud,
+          convenientes y muy accesibles donde podra obtener un prestamo personal
+          si lo califica
+        </p>
+        <a href="#">leer mas</a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
