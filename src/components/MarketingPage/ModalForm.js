3c703a09fcd1c0105669e5d8c17bcf5729d8992c
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-08-22 12:27:17 -0700

    added form needed for client

--- src/components/MarketingPage/ModalForm.js
+++ src/components/MarketingPage/ModalForm.js
@@ -34,58 +34,58 @@ const ModalForm = () => {
               <h3>informacion de los propietarios</h3>
               {/***************** START OF USER FIELDS *********************/}
               {/** first user */}
-              <label htmlFor="name" className="nombre">
+              <label htmlFor="first-name" className="nombre">
                 1. Nombre:
-                <input type="text" id="name" name="name" />
+                <input type="text" id="first-name" name="first-name" />
               </label>
               <div className="phone-info">
-                <label htmlFor="phone" className="phone">
+                <label htmlFor="first-phone" className="phone">
                   Tel:
-                  <input type="number" name="phone" id="phone" />
+                  <input type="number" name="first-phone" id="first-phone" />
                 </label>
                 <div className="taxes">
                   <span>Declara Impuestos?</span>
-                  <label htmlFor="yes">
-                    <input type="checkbox" name="yes" id="yes" />
+                  <label htmlFor="first-yes">
+                    <input type="checkbox" name="first-yes" id="first-yes" />
                     Si
                   </label>
-                  <label htmlFor="no">
-                    <input type="checkbox" name="no" id="no" />
+                  <label htmlFor="first-no">
+                    <input type="checkbox" name="first-no" id="first-no" />
                     No
                   </label>
                 </div>
               </div>
               {/** email */}
-              <label htmlFor="email" className="email">
+              <label htmlFor="first-email" className="email">
                 Correo electronico:
-                <input type="email" name="email" id="email" />
+                <input type="email" name="first-email" id="first-email" />
               </label>
               {/** second user */}
 
-              <label htmlFor="name" className="nombre">
+              <label htmlFor="second-name" className="nombre">
                 1. Nombre:
-                <input type="text" id="name" name="name" />
+                <input type="text" id="second-name" name="second-name" />
               </label>
               <div className="phone-info">
-                <label htmlFor="phone" className="phone">
+                <label htmlFor="second-phone" className="phone">
                   Tel:
-                  <input type="number" name="phone" id="phone" />
+                  <input type="number" name="second-phone" id="second-phone" />
                 </label>
                 <div className="taxes">
                   <span>Declara Impuestos?</span>
-                  <label htmlFor="yes">
-                    <input type="checkbox" name="yes" id="yes" />
+                  <label htmlFor="second-yes">
+                    <input type="checkbox" name="second-yes" id="second-yes" />
                     Si
                   </label>
-                  <label htmlFor="no">
-                    <input type="checkbox" name="no" id="no" />
+                  <label htmlFor="second-no">
+                    <input type="checkbox" name="second-no" id="second-no" />
                     No
                   </label>
                 </div>
               </div>
-              <label htmlFor="email" className="email">
+              <label htmlFor="second-email" className="email">
                 Correo electronico:
-                <input type="email" name="email" id="email" />
+                <input type="email" name="second-email" id="second-email" />
               </label>
               {/** end of second user */}
               <label htmlFor="address" className="address1">
@@ -109,6 +109,176 @@ const ModalForm = () => {
             </div>
           </div>
           {/***************** END OF USER FIELDS *********************/}
+          {/***************** START OF PROPERTY INORMATION *********************/}
+          <div className="property-info">
+            <h3>informacion de la propiedad</h3>
+            {/********* year, sqr foot and total floors *********/}
+            <div className="house">
+              <label htmlFor="year">
+                Ano construida:
+                <input type="text" name="year" id="year" />
+              </label>
+              <label htmlFor="sqrFoot">
+                Pies cuadrados:
+                <input type="text" name="sqrFoot" id="sqrFoot" />
+              </label>{" "}
+              <label htmlFor="totalFloors">
+                Cuantos pisos:
+                <input type="text" name="totalFloors" id="totalFloors" />
+              </label>
+            </div>
+            {/********* end of year, sqr foot and total floors *********/}
+            <div className="construction-type">
+              <div className="year-built">
+                <span>Tipo de construccion:</span>
+                <div className="checkbox-wrapper">
+                  <label htmlFor="residence">
+                    <input type="checkbox" name="residence" id="residence" />
+                    Residencia familiar
+                  </label>
+                  <label htmlFor="town-house">
+                    <input type="checkbox" name="town-house" id="town-house" />
+                    Town house
+                  </label>{" "}
+                  <label htmlFor="condominio">
+                    <input type="checkbox" name="condominio" id="condominio" />
+                    Condominio
+                  </label>
+                  <label htmlFor="mobile-home">
+                    <input
+                      type="checkbox"
+                      name="mobile-home"
+                      id="mobile-home"
+                    />
+                    Mobile house
+                  </label>
+                </div>
+              </div>
+            </div>
+            {/********* end of construction type *********/}
+            <div className="ceiling-type">
+              <div className="year-built">
+                <span>Tipo de techo:</span>
+                <div className="checkbox-wrapper">
+                  <label htmlFor="shingle">
+                    <input type="checkbox" name="shingle" id="shingle" />
+                    Shingle
+                  </label>
+                  <label htmlFor="teja">
+                    <input type="checkbox" name="teja" id="teja" />
+                    Teja
+                  </label>{" "}
+                  <label htmlFor="barro">
+                    <input type="checkbox" name="barro" id="barro" />
+                    Barro
+                  </label>
+                  <label htmlFor="madera">
+                    <input type="checkbox" name="madera" id="madera" />
+                    Madera
+                  </label>
+                  <label htmlFor="plano">
+                    <input type="checkbox" name="plano" id="plano" />
+                    Plano
+                  </label>
+                </div>
+              </div>
+            </div>
+            {/********* end of ceiling type *********/}
+            <div className="ceiling-condition">
+              <div className="year-built">
+                <span>Condicion del techo:</span>
+                <div className="checkbox-wrapper">
+                  <label htmlFor="exelente">
+                    <input type="checkbox" name="exelente" id="exelente" />
+                    Exelente
+                  </label>
+                  <label htmlFor="bueno">
+                    <input type="checkbox" name="bueno" id="bueno" />
+                    Bueno
+                  </label>{" "}
+                  <label htmlFor="pasable">
+                    <input type="checkbox" name="pasable" id="pasable" />
+                    Pasable
+                  </label>
+                  <label htmlFor="viejo">
+                    <input type="checkbox" name="viejo" id="viejo" />
+                    Viejo
+                  </label>
+                </div>
+              </div>
+            </div>
+            {/********* end of condition type *********/}
+            <div className="sombreado-techo">
+              <div className="year-built">
+                <span>Sombreado de techo:</span>
+                <div className="checkbox-wrapper">
+                  <label htmlFor="nada">
+                    <input type="checkbox" name="nada" id="nada" />
+                    Nada
+                  </label>
+                  <label htmlFor="poco">
+                    <input type="checkbox" name="poco" id="poco" />
+                    Poco
+                  </label>{" "}
+                  <label htmlFor="mucho">
+                    <input type="checkbox" name="mucho" id="mucho" />
+                    Mucho
+                  </label>
+                </div>
+              </div>
+            </div>
+            {/********* end of sombreado de techo *********/}
+            <div className="pool">
+              <div className="year-built">
+                <span>Tiene alberca?:</span>
+                <div className="checkbox-wrapper">
+                  <label htmlFor="withPool">
+                    <input type="checkbox" name="withPool" id="withPool" />
+                    Si
+                  </label>
+                  <label htmlFor="noPool">
+                    <input type="checkbox" name="noPool" id="noPool" />
+                    No
+                  </label>
+                </div>
+              </div>
+            </div>
+            {/********* end of contains pool? *********/}
+            <div className="gate">
+              <span>Gate: contrasena entrada</span>
+              <label htmlFor="gate-number">
+                <input type="text" id="gate-number" name="gate-number" />
+                (se require para hacer el site-survey)
+              </label>
+            </div>
+            {/********* end of gate *********/}
+            <div className="HOA">
+              <span>HOA:</span>
+              <label htmlFor="yes-hoa" className="yes-hoa">
+                <input type="checkbox" name="yes-hoa" id="yes-hoa" />
+                Si
+              </label>
+              <label htmlFor="no-hoa" className="yes-hoa">
+                <input type="checkbox" name="no-hoa" id="no-hoa" />
+                No
+              </label>
+              <label htmlFor="hoa-info" className="important hoa-info">
+                HOA INFO:
+                <input type="text" name="hoa-info" id="hoa-info" />
+              </label>
+              <label htmlFor="hoa-phone" className="important">
+                Tel:
+                <input type="number" name="hoa-phone" id="hoa-phone" />
+              </label>
+            </div>
+          </div>
+          {/********* end of HOA *********/}
+
+          <div className="update-file">
+            <label htmlFor="pdf">
+              <input type="file" name="pdf" id="pdf" />
+            </label>
+          </div>
           <button>enviar</button>
         </form>
       </Modal>
