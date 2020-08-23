58303280b1aded2dad3db9af3710d823a1fff9cd
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-08-23 11:57:55 -0700

    adding about page

--- src/components/MarketingPage/MainHomePage.js
+++ src/components/MarketingPage/MainHomePage.js
@@ -1,16 +1,12 @@
 import React from "react";
-import Navbar from "./Navbar";
 import Header from "./Header";
 import Content from "./Content";
-import Footer from "./Footer";
 
 const MainHomePage = () => {
   return (
     <div>
-      <Navbar />
       <Header />
       <Content />
-      <Footer />
     </div>
   );
 };
---
+++ src/components/MarketingPage/about/AboutHeader.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const AboutHeader = () => {
+  return (
+    <header className="AboutHeader">
+      <div className="AboutHeader-wrapper">
+        {/**
+         <h1>sobre gift solar</h1>
+           <p>
+          Ofrecemos 3 exelentes paquetes donde incluye un extra para su salud,
+          convenientes y muy accesibles donde podra obtener un prestamo personal
+          si lo califica
+        </p>
+        <a href="#">leer mas</a>
+      */}
+      </div>
+    </header>
+  );
+};
+
+export default AboutHeader;
